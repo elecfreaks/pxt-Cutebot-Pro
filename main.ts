ac87cca092cd73ff563e31830862ee3c2d83e3d8
zhuyu <zhuyu@elecfreaks.com>
2024-08-30 15:22:29 +0800

    兼容测试

--- main.ts
+++ main.ts
@@ -720,7 +720,7 @@ namespace CutebotPro {
         } else {
             return cutebotProV1.readVersions();
         }
-        
+
     }
 
     let version = -1;
@@ -730,16 +730,6 @@ namespace CutebotPro {
             let i2cBuffer = pins.createBuffer(7);
             i2cBuffer[0] = 0x99;
             i2cBuffer[1] = 0x15;
-            i2cBuffer[2] = 0x00;
-            i2cBuffer[3] = 0x00;
-            i2cBuffer[4] = 0x00;
-            i2cBuffer[5] = 0x00;
-            i2cBuffer[6] = 0x88;
-            pins.i2cWriteBuffer(0x10, i2cBuffer)
-            version = pins.i2cReadNumber(0x10, NumberFormat.UInt8LE, false);
-
-            i2cBuffer[0] = 0x99;
-            i2cBuffer[1] = 0x15;
             i2cBuffer[2] = 0x01;
             i2cBuffer[3] = 0x00;
             i2cBuffer[4] = 0x00;
@@ -747,6 +737,16 @@ namespace CutebotPro {
             i2cBuffer[6] = 0x88;
             pins.i2cWriteBuffer(0x10, i2cBuffer)
             version = pins.i2cReadNumber(0x10, NumberFormat.UInt8LE, false);
+
+            // i2cBuffer[0] = 0x99;
+            // i2cBuffer[1] = 0x15;
+            // i2cBuffer[2] = 0x01;
+            // i2cBuffer[3] = 0x00;
+            // i2cBuffer[4] = 0x00;
+            // i2cBuffer[5] = 0x00;
+            // i2cBuffer[6] = 0x88;
+            // pins.i2cWriteBuffer(0x10, i2cBuffer)
+            // version = pins.i2cReadNumber(0x10, NumberFormat.UInt8LE, false);
         }
         return version;
         // return 2;
